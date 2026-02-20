b954850a10b608b486357095a504bc3fc11f24af
Sagor8S <sagorhowlader.edu@gmail.com>
2026-02-20 20:30:41 +0600

    The Project html css and javascript with make.

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ const loginLink = document.querySelector('.login-link');
 registerLink.onclick = () => {
     wrapper.classList.add('active');
 }
+
+
+loginLink.onclick = () => {
+    wrapper.classList.remove('active');
+}
